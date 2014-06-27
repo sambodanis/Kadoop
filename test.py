58fbdd69fdb472e58da2f23169fba79924ec9825
sambodanis <sam.bodanis@gmail.com>
2014-06-27 14:51:08 -0700

    code now nice

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 json_header = {'content-type': 'application/json'}
 
-url = 'http://ec2-54-183-120-129.us-west-1.compute.amazonaws.com/'
+url = 'http://ec2-54-183-120-129.us-west-1.compute.amazonaws.com:5000/'
 # url = 'http://localhost:5000/work'
 
 r = requests.post(url=url,
---
+++ views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, redirect, render_template, url_for, jsonify
+from flask.views import MethodView
+
+# from flask.ext.mongoengine.wtf import model_form
+# from server.models import User, Debt, Purchase
+from Kadoop import app
+import json
+import datetime
+
+
+class WorkAPI(MethodView):
+
+    def get(self):
+        return jsonify({'res': True})
+
+    def post(self):
+
+        data = request.form
+        print data, '2'
+        return jsonify({'res': True})
+
+
+app.add_url_rule('/work/', view_func=WorkAPI.as_view('work'))
+# app.add_url_rule('/purchases/', view_func=PurchaseAPI.as_view('purchases'))
