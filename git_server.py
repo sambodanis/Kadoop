094155117088b9c8a70c8b2537e02caf67d84545
sambodanis <sam.bodanis@gmail.com>
2014-06-27 16:10:27 -0700

    pull server fixed

--- git_server.py
+++ git_server.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def pull():
-	os.system('git pull')
+    os.system('git pull')
+
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=5001)
+    app.run(host='0.0.0.0', port=5001)
