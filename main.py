362bbffcd8222ec8e26f44c9a0f704149bfab9f7
sambodanis <sam.bodanis@gmail.com>
2014-06-27 13:56:28 -0700

    syntax errors fixed

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def hello():
 @app.route("/work", methods=['GET', 'POST'])
 def get_work():
     print request.data
-    return "Hello"
+    return {'res': True, 'work': [1, 2, 3, 4, 5, 6]}
 
 
 @app.route("/complete", methods=['POST'])
 def done_work():
-
+    return {'res': True, 'msg': 'look out for potatoes'}
 
 if __name__ == "__main__":
     app.run()
