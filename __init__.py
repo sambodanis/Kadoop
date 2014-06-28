4df72375f8b880797007711e7d9f06653df16a01
sambodanis <sam.bodanis@gmail.com>
2014-06-28 08:56:10 -0700

    landing page added

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
 from flask.ext.mongoengine import MongoEngine
@@ -11,7 +11,7 @@ settings_path = 'config/settings.json'
 settings = json.loads(open(settings_path, 'r').read())
 db_settings = settings['db_settings']
 
-app = Flask(__name__)
+app = Flask(__name__)  # , static_url_path='')
 app.config["MONGODB_SETTINGS"] = {"DB": db_settings['DB']}
 app.config["SECRET_KEY"] = db_settings["SECRET"]
 
@@ -26,29 +26,10 @@ db = MongoEngine(app)
 import Kadoop.views
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template('kadoop_form.html')
+# return url_for('static', filename='kadoop_form.html')
 
 if __name__ == '__main__':
     app.run()
